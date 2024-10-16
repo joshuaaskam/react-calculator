8e1f19637fc57abb6867d0aa84784067db85ef88
joshuaaskam <115502612+joshuaaskam@users.noreply.github.com>
2024-10-16 15:48:35 -0700

    added grid styling and output styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import './styles.css';
 
 function App() {
   return (
-    <div className='css-grid'>
+    <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'></div>
-        <div className='current-operand'></div>
+        <div className='previous-operand'>1,000 * </div>
+        <div className='current-operand'>200000000</div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
